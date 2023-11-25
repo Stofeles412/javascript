f26799a2d9029ceb28abc727b4ef7c46b3a6378a
Stofeles412 <fernandostofeles@gmail.com>
2023-11-25 14:08:53 -0300

    estudei condiçoes

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel > 60 ){//condição simples
+    console.log('você ultrapassou a velocidade permetida, MULTADO!')
+}
+console.log ('diriga sempre usando sito de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'brasil'
+if(pais == 'brasil'){
+    console.log(`vivendo no ${pais} voce é brasileiro`)
+}else{
+    console.log(`vivendo no pais ${pais} voce é estrangeiro`)
+}
