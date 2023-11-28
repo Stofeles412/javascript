e8b17cce1749692b42c7424c8fa2ab3b783bf3af
Stofeles412 <fernandostofeles@gmail.com>
2023-11-28 18:39:19 -0300

    atualizei js

--- aula012ex/modelo/script.js
+++ aula012ex/modelo/script.js
@@ -2,11 +2,15 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById ('foto')
     var data = new Date()
-    var hora = data.getHours()
+    var hora = 8
     var min = data.getMinutes()
     msg.innerHTML = (`agora são ${hora}:${min} horas`)
-    if (hora >=0 && hora < 12){
-        
-    }
+    if (hora >=0 && hora < 12){ img.src = 'manha.png'
+
+}else if(hora >= 12 && hora < 18){ img.scr = 'tarde.png'
+
     
+}else{
+    img.src = 'noite.png'
+}
 }
