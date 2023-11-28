f0af154f3927227fbf38e9dfa714e622492de04c
Stofeles412 <fernandostofeles@gmail.com>
2023-11-28 16:53:53 -0300

    estudando

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,8 @@
-alert ('ola')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById ('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = (`agora são ${hora} horas`)
+    
+}
--- aula012ex/modelo/script.js
+++ aula012ex/modelo/script.js
@@ -1,6 +1,12 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById ('foto')
-    var data = new Data()
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    msg.innerHTML = (`agora são ${hora}:${min} horas`)
+    if (hora >=0 && hora < 12){
+        
+    }
     
 }
