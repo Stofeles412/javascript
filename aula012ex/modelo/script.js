e215d1f9fbcacf9f8c50ff965463cd419705b80c
Stofeles412 <fernandostofeles@gmail.com>
2023-11-28 22:30:30 -0300

    estudando

--- aula012ex/modelo/script.js
+++ aula012ex/modelo/script.js
@@ -2,15 +2,15 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById ('foto')
     var data = new Date()
-    var hora = 8
+    var hora = 12
     var min = data.getMinutes()
-    msg.innerHTML = (`agora são ${hora}:${min} horas`)
-    if (hora >=0 && hora < 12){ img.src = 'manha.png'
+    msg.innerHTML = (`agora são ${hora}:${min} - horas`)
+}if(hora >= 0 && hora < 12){
+    //bom dia
 
-}else if(hora >= 12 && hora < 18){ img.scr = 'tarde.png'
-
-    
+}else if(hora >= 12 && hora < 18){
+    //boa tarde
 }else{
-    img.src = 'noite.png'
-}
+    //boa noite
+    
 }
