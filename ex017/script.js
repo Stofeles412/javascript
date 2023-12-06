f7bd80fbcb8ee9c5aaef66a7bae0df1819a7f539
Stofeles412 <fernandostofeles@gmail.com>
2023-12-06 12:09:40 -0300

    conseguir criar a tabuada com js

---
+++ ex017/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+   var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById('seltab')
+if(num.value.length == 0){
+  
+    window.alert ('[ERRO!] por favor digite algum numero.')
+}else{
+    var n = Number(num.value)
+    var c = 1
+    while (c <= 10){
+
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
+    }
+   
+}
+   
+}
