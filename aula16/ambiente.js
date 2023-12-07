7d882c285a7dc3f66ca147e82199d33652c49d35
Stofeles412 <fernandostofeles@gmail.com>
2023-12-07 13:33:26 -0300

    estudei nesta tarde

--- aula16/ambiente.js
+++ aula16/ambiente.js
@@ -1,5 +1,13 @@
-let num = [3,7,8,2,1] 
+let num = [3,7,8,2,1,] 
 num.push(1)
-num.sort()
+//num.sort()
 //console.log  (`nossos ventores são ${num.length}`)
 console.log (num[2])
+var pos = num.indexOf(9)
+
+if(pos == -1){
+    console.log ('o valor nao foi encontardo')
+}else{
+    console.log (`o valor estar na posiçãoo ${pos}`)
+
+}
